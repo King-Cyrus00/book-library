d32642ece36f293dc6a04ed40be376a1394bdfa8
King-Cyrus00 <cyrus.barrow07@gmail.com>
2025-06-07 22:04:03 +0100

    updated  app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import BookDetail from "./pages/BookDetail";
+import EditBook from "./pages/EditBook";
+import AddBook from "./pages/AddBook";
+import NotFound from "./pages/NotFound";
 
+const eventHiveRouter = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/register", element: <Register /> },
+  { path: "/login", element: <Login /> },
+  { path: "/book-detail/", element: <BookDetail /> },
+  { path: "/edit-book/", element: <EditBook /> },
+  { path: "/add-book/", element: <AddBook /> },
+  { path: "*", element: <NotFound /> }
 
+]);
 
-function App() {
+export default function App() {
   return (
     <>
-     <h1>Welcome to Team Bambi</h1>
+      <RouterProvider router={eventHiveRouter} />
     </>
-  )
+  );
 }
 
-export default App
+
